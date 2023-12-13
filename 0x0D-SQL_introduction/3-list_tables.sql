6407e0ab430308e55d3bdb97d4ecd7a3e292f509
gregorymark254 <gregorymark254@gmail.com>
2023-12-13 18:29:08 +0300

    show

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,5 @@
---script that lists all the tables of a database in your MySQL server.
---The database name will be passed as argument of mysql command
+-- script that lists all the tables of a database in your MySQL server.
+-- The database name will be passed as argument of mysql command
 
 SHOW TABLES;
 
