da7e14bd7f60533e1686696258451c054e0cde70
gregorymark254 <gregorymark254@gmail.com>
2023-12-14 23:11:14 +0300

    subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,5 +4,4 @@
 -- Results must be sorted in ascending order by cities.id
 -- You are not allowed to use the JOIN keyword
 
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name='Carlifornia') ORDER BY id ASC;
-
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
